ed1b27ee79d9cf8e32fa2a72d2cc60d808bd3886
puckaua <puckaua@gmail.com>
2025-08-06 11:41:42 -0300

    Add files via upload

---
+++ exercicio01.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio01
+{
+    internal class exercicio01
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Digite um número positivo inteiro: ");
+            n = int.Parse(Console.ReadLine());
+            int num = 1;
+            int linha = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < linha; j++)
+                {
+                    Console.Write(num + " ");
+                    num++;
+                }
+                Console.WriteLine();
+                linha++;
+            }
+        }
+    }
+}
